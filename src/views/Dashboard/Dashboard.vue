87dde5b4ebea6a32240f948866247915b1cbd41f
alfonso.mateos <aj.mateos97@gmail.com>
2021-08-03 20:29:36 +0100

    Creates Dashboard view

---
+++ src/views/Dashboard/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    Dashboard
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+
+};
+</script>
+
+<style src="./style.scss" scoped>
+
+</style>
