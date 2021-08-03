69f44cad0fdfecc27b193955b655a93721cac411
alfonso.mateos <aj.mateos97@gmail.com>
2021-08-03 18:02:26 +0100

    Adds vue3-table-lite to the project and all necessary changes for it

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,12 @@ library.add(fas);
 // Global styles
 import "./assets/scss/flex-styles.scss";
 
+// Vue Table lite
+import VueTableLite from "vue3-table-lite";
+
 createApp(App)
   .use(store)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
+  .component("vue-table-lite", VueTableLite)
   .mount("#app");
